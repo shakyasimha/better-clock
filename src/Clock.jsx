f1f66f4b397532b1f8e0a5367daa2b12b04242aa
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2024-08-27 15:52:38 +0545

    added clock icon; added clock component

---
+++ src/Clock.jsx
@@ -0,0 +1,15 @@
+function Clock() {
+    return (
+        <>
+            <div className="clock">
+                <p> 00 </p>
+                <p> : </p>
+                <p> 00 </p>
+                <p> : </p>
+                <p> 00 </p>
+            </div>
+        </>
+    )
+}
+
+export default Clock; 
